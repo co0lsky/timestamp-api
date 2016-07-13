66f4dd41a022fdb68e0842aa91e5ad44c71eceb5
Sky <sky4just@gmail.com>
2016-07-13 15:34:37 +0000

    Complete

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require("express")
+var moment = require("moment")
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:date', function (req, res) {
+  var param = isNaN(req.params.date) ? req.params.date : req.params.date * 1000
+  var date = moment(new Date(param))
+  
+  // if (isNaN(date))
+  
+  res.send(JSON.stringify({
+    unix: date.format('X'),
+    natural: date.format("MMMM D, YYYY")
+  }));
+  
+  // { "unix": 1450137600, "natural": "December 15, 2015" }
 });
 
 app.listen(8080, function () {
